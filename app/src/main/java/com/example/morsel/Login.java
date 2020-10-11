67f8a364ac54172deb1531abe4e068e60f118362
random3900 <roopavidhyaa@gmail.com>
2020-10-12 01:18:43 +0530

    Add Login with Firebase

--- app/src/main/java/com/example/morsel/Login.java
+++ app/src/main/java/com/example/morsel/Login.java
@@ -2,20 +2,31 @@ package com.example.morsel;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 public class Login extends Fragment {
+    private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,17 +36,14 @@ public class Login extends Fragment {
 
         final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
         final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        final TextInputLayout usernameTextInput = view.findViewById(R.id.username_text_input);
+        final TextInputEditText usernameEditText = view.findViewById(R.id.username_edit_text);
         MaterialButton nextButton = view.findViewById(R.id.next_button);
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isPasswordValid(passwordEditText.getText())) {
-                    passwordTextInput.setError(getString(R.string.morsel_error_password));
-                } else {
-                    passwordTextInput.setError(null); // Clear the error
-                    //((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false);
-                }
+            signIn(usernameEditText.getText().toString(), passwordEditText.getText().toString());
             }
         });
 
@@ -49,11 +57,41 @@ public class Login extends Fragment {
             }
 
         });
+        mAuth = FirebaseAuth.getInstance();
+        String password;
+        String email;
+
         return view;
     }
 
     private boolean isPasswordValid(@Nullable Editable text) {
         return text != null && text.length() >= 8;
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Login", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getActivity(), "Task Successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Login", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+
     }
 
 }
--- app/src/main/java/com/example/morsel/MainActivity.java
+++ app/src/main/java/com/example/morsel/MainActivity.java
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements NavigationHost{
+    private FirebaseAuth mAuth;
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Toast.makeText(getApplicationContext(), "User exists", Toast.LENGTH_SHORT);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
