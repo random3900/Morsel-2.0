19835cbfff3efc84d601ab034373b3edcc02efbd
sudarshana06 <68108766+sudarshana06@users.noreply.github.com>
2020-10-13 01:04:47 +0530

    Create SecondScreen.java

---
+++ app/src/main/java/com/example/morsel/SecondScreen.java
@@ -0,0 +1,39 @@
+package com.example.morsel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+public class SecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_screen);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_file, menu);
+        return true;
+    }
+
+    public void onClickLocation(View V){
+        Intent i = new Intent(this, location.class);
+        startActivity(i);
+    }
+    public void onClickContribute(View V){
+        Intent i = new Intent(this, donate.class);
+        startActivity(i);
+    }
+    public void onClickVolunteer(View V){
+        Intent i = new Intent(this, volunteer.class);
+        startActivity(i);
+    }
+
+
+}
