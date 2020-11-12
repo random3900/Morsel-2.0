65919bb831c50593855d134d32cdec13df11a4e6
aishwar18 <sraishusr@gmail.com>
2020-11-12 09:56:25 +0530

    Updated after volunteer pages

---
+++ app/src/main/java/com/example/morsel/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.example.morsel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
+}
